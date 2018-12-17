d18dd32a788bc698cf7d5e0a5045d5485403dd3b
vinitsk <kallianpur.vinit@gmail.com>
2018-12-17 15:44:47 -0500

    Improvements Angular JWT

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 
 
@@ -13,6 +14,8 @@ import {map} from 'rxjs/operators';
 export class AuthService {
 
 baseUrl = 'http://localhost:5000/api/auth/';
+jwtHelper = new JwtHelperService();
+decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -24,6 +27,8 @@ map((response: any) => {
   const user = response;
   if (user) {
     localStorage.setItem('token', user.token);
+    this.decodedToken = this.jwtHelper.decodeToken(user.token);
+    console.log(this.decodedToken);
 }
 
 }
@@ -32,6 +37,12 @@ map((response: any) => {
 
 register(model: any){
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
 
  model: any = {};
 
-  constructor(private authService: AuthService,private alertify: AlertifyService) { }
+  constructor(public authService: AuthService,private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -26,8 +26,7 @@ this.alertify.success('Logged in successfully');
   }
 
   loggedIn() {
-  const token = localStorage.getItem('token');
-  return !!token;
+return this.authService.loggedIn();
   }
 
 
